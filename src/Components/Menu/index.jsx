f213130514d987d58d7b4d17c49fb9e9ebcf2b68
Laís Sousa <lais.csousa@spread.com.br>
2023-07-07 16:39:42 -0300

    criação de icones menu

--- src/Components/Menu/index.jsx
+++ src/Components/Menu/index.jsx
@@ -1,4 +1,10 @@
 import styles from './Menu.module.scss'
+import funcionalidade from './icones/funcionalidade.svg'
+import home from './icones/home.svg'
+import interativo from './icones/interativo.svg'
+import projetos from './icones/projetos.svg'
+import tecnologias from './icones/tecnologias.svg'
+
 
 const Menu = () =>{
 
@@ -6,18 +12,23 @@ return(
     <nav className={styles.menu}>
         <ul className={styles.menu__lista}>
             <li className={styles.menu__item}>
+            <img src={home} alt='Inicio'/>
              <a href='/'>Inicio</a>   
             </li>
             <li className={styles.menu__item}>
+            <img src={projetos} alt='Projetos'/>
              <a href='/'>Projetos</a>   
             </li>
             <li className={styles.menu__item}>
+            <img src={funcionalidade} alt='Funcionalidades'/>
              <a href='/'>Funcionalidades</a>    
             </li>
             <li className={styles.menu__item}>
+            <img src={interativo} alt='Interativos'/>
              <a href='/'>Interativos</a>    
             </li>
             <li className={styles.menu__item}>
+            <img src={tecnologias} alt='Tecnologias'/>
              <a href='/'>Tecnologias</a>    
             </li>
         </ul>
